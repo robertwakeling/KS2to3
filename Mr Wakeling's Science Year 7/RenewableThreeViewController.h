d2df61c265aca16e393e5d9f40bae1761430f991
Robert Wakeling <robertwakeling82@gmail.com>
2012-07-26 22:37:14 +0100

    Just finished Renewable

--- Mr Wakeling's Science Year 7/RenewableThreeViewController.h
+++ Mr Wakeling's Science Year 7/RenewableThreeViewController.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Tester.h"
 
-@interface RenewableThreeViewController : UIViewController
+@interface RenewableThreeViewController : UIViewController <UIAlertViewDelegate, UITextFieldDelegate>
+{
+    int theScore;
+    
+    // To move the view up and down for text fields
+    BOOL viewShiftedForKeyboard;
+    NSTimeInterval keyboardSlideDuration;
+    CGFloat keyboardShiftAmount;
+    NSArray *textfieldThatNeedsMoving;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *solar;
+@property (weak, nonatomic) IBOutlet UITextField *wind;
+@property (weak, nonatomic) IBOutlet UITextField *tidal;
+
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 
 @end
--- Mr Wakeling's Science Year 7/RenewableThreeViewController.m
+++ Mr Wakeling's Science Year 7/RenewableThreeViewController.m
@@ -13,6 +13,10 @@
 @end
 
 @implementation RenewableThreeViewController
+@synthesize solar;
+@synthesize wind;
+@synthesize tidal;
+@synthesize scoreLabel;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,8 +33,149 @@
 	// Do any additional setup after loading the view.
 }
 
+-(IBAction)checkAnswers
+{
+    int a, b, c;
+    
+    if ([solar.text isEqualToString:@"solar"]) {
+        a = 1;
+    }
+    else {
+        a = 0;
+    }
+    
+    if ([wind.text isEqualToString:@"wind"]) {
+        b = 1;
+    }
+    else {
+        b = 0;
+    }
+    if ([tidal.text isEqualToString:@"tidal"]) {
+        c = 1;
+    }
+    else {
+        c = 0;
+    }
+    theScore = a + b + c;
+    scoreLabel.text = [NSString stringWithFormat:@"%i/3", theScore];
+    
+    if (theScore <= 1) {
+        scoreLabel.textColor = [UIColor redColor];
+    }
+    if (theScore == 2) {
+        scoreLabel.textColor = [UIColor orangeColor];
+    }
+    if (theScore == 3) {
+        scoreLabel.textColor = [UIColor greenColor];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Congratulations!" message:@"You know some examples of renewable energy!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    
+    [[NSNotificationCenter defaultCenter] addObserver: self
+                                             selector: @selector(shiftViewUpForKeyboard:)
+                                                 name: UIKeyboardWillShowNotification
+                                               object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver: self
+                                             selector: @selector(shiftViewDownAfterKeyboard)
+                                                 name: UIKeyboardWillHideNotification
+                                               object: nil];
+    
+    
+}
+
+- (void) viewDidDisappear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name: UIKeyboardWillShowNotification
+                                                  object: nil];
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name: UIKeyboardWillHideNotification
+                                                  object: nil];
+    
+    
+}
+
+- (NSArray *) LowTextFieldsThatNeedTheViewToMove
+{
+    textfieldThatNeedsMoving = [[NSArray alloc] initWithObjects:tidal, nil];
+    return textfieldThatNeedsMoving;
+}
+
+- (void) shiftViewUpForKeyboard: (NSNotification*)theNotification;
+{
+    
+    
+    NSEnumerator *enumerator = [[self LowTextFieldsThatNeedTheViewToMove] objectEnumerator];
+    UITextField *object;
+    
+    while ((object = [enumerator nextObject])) {
+        // do something with object...
+        if (object.isEditing) {
+            
+            CGRect keyboardFrame;
+            NSDictionary* userInfo = theNotification.userInfo;
+            keyboardSlideDuration = [[userInfo objectForKey: UIKeyboardAnimationDurationUserInfoKey] floatValue];
+            keyboardFrame = [[userInfo objectForKey: UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+            
+            UIInterfaceOrientation theStatusBarOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+            
+            if UIInterfaceOrientationIsLandscape(theStatusBarOrientation)
+                keyboardShiftAmount = keyboardFrame.size.width;
+            else
+                keyboardShiftAmount = keyboardFrame.size.height;
+            
+            [UIView beginAnimations: @"ShiftUp" context: nil];
+            [UIView setAnimationDuration: keyboardSlideDuration];
+            self.view.center = CGPointMake( self.view.center.x, self.view.center.y - keyboardShiftAmount);
+            [UIView commitAnimations];
+            viewShiftedForKeyboard = TRUE;
+            
+        }
+    }
+}
+
+
+
+//------------------
+
+- (void) shiftViewDownAfterKeyboard;
+{
+    if (viewShiftedForKeyboard)
+    {
+        [UIView beginAnimations: @"ShiftUp" context: nil];
+        [UIView setAnimationDuration: keyboardSlideDuration];
+        self.view.center = CGPointMake( self.view.center.x, self.view.center.y + keyboardShiftAmount);
+        [UIView commitAnimations];
+        viewShiftedForKeyboard = FALSE;
+    }
+}
+
+
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return NO;
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    Tester *detail = [self.storyboard instantiateViewControllerWithIdentifier:@"Overall1"];
+    [self.navigationController pushViewController:detail animated:YES];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"Renewable3" object:self];
+}
+
 - (void)viewDidUnload
 {
+    [self setSolar:nil];
+    [self setWind:nil];
+    [self setTidal:nil];
+    [self setScoreLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
