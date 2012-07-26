2321d292076241b399e17ee77798ad93d551f864
Robert Wakeling <robertwakeling82@gmail.com>
2012-07-26 18:02:46 +0100

    After all the errors, it seems to be working…  Renewable started

---
+++ Mr Wakeling's Science Year 7/RenewableThreeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RenewableThreeViewController.h
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 26/07/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RenewableThreeViewController : UIViewController
+
+@end
---
+++ Mr Wakeling's Science Year 7/RenewableThreeViewController.m
@@ -0,0 +1,43 @@
+//
+//  RenewableThreeViewController.m
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 26/07/2012.
+//
+//
+
+#import "RenewableThreeViewController.h"
+
+@interface RenewableThreeViewController ()
+
+@end
+
+@implementation RenewableThreeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
