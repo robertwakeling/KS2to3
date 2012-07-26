932837195117985ecbccb0db07a62a2eda99b91e
Robert Wakeling <robertwakeling82@gmail.com>
2012-07-27 00:30:56 +0100

    Half way through creating the dreaded seasons page

---
+++ Mr Wakeling's Science Year 7/AdaptationThreeViewController.h
@@ -0,0 +1,19 @@
+//
+//  AdaptationThreeViewController.h
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 27/07/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "Tester.h"
+
+@interface AdaptationThreeViewController : UIViewController <UIAlertViewDelegate, UITextFieldDelegate>
+{
+    int theScore;
+    
+    NSArray *textfieldThatNeedsMoving;
+}
+
+@end
---
+++ Mr Wakeling's Science Year 7/AdaptationThreeViewController.m
@@ -0,0 +1,51 @@
+//
+//  AdaptationThreeViewController.m
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 27/07/2012.
+//
+//
+
+#import "AdaptationThreeViewController.h"
+
+@interface AdaptationThreeViewController ()
+
+@end
+
+@implementation AdaptationThreeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+-(IBAction)checkAnswers
+{
+    Tester *detail = [self.storyboard instantiateViewControllerWithIdentifier:@"Overall1"];
+    [self.navigationController pushViewController:detail animated:YES];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"Adaptation3" object:self];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
